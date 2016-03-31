2137d5b9770f967898b28df6e8017a819e088895
alGatto <gattuso.al@gmail.com>
2016-03-31 21:35:48 +0200

    index+menu

---
+++ index.php
@@ -0,0 +1,16 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>My Resume</title>
+    </head>
+    <body>
+        <?php include("menu.php");?>
+        <h1>My resume</h1>
+        <h2><?php echo "Bonjour voici ma "; ?>Page test</h2>
+        <ul>
+            <li style="color: aqua">Acqua blue text</li>
+            <li style="color: darkred">Red text</li>
+        </ul>
+
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<html>
+    <div>
+        <ul>
+            <li><a href="index.php">Page1</a></li>
+            <li>Page 2</li>
+            <li>Page 3</li>
+        </ul>
+    </div>
+</html>
